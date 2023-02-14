f7ee6d056edaf839ff22c1a4e0ca4a921095f344
Inna_IT <leto2077@gmail.com>
2023-02-15 00:29:28 +0300

    ДЗ № 3 в одном месте

--- DZ-Csh-3-1/Program.cs
+++ DZ-Csh-3-1/Program.cs
@@ -1,4 +1,4 @@
-﻿// Задача 19
+﻿// -------Задача 19------
 // Напишите программу, которая принимает на вход пятизначное число
 //  и проверяет, является ли оно палиндромом.
 // 14212 -> нет
@@ -25,3 +25,45 @@
 //         Console.WriteLine("Введено не 5-значное число");
 //     }
 // }
+
+
+// -------Задача 21-----
+// Напишите программу, которая принимает на вход координаты двух 
+// точек и находит расстояние между ними в 3D пространстве.
+// A (3,6,8); B (2,1,-7), -> 15.84
+// A (7,-5, 0); B (1,-1,9) -> 11.53
+
+// int ImputNum(string strxyz) 
+// {
+//     System.Console.WriteLine(strxyz);
+//     return Convert.ToInt32(Console.ReadLine());
+// }
+
+// int x1 = ImputNum("Enter ax: "); 
+// int y1 = ImputNum("Enter ay: ");
+// int z1 = ImputNum("Enter az: ");
+// int x2 = ImputNum("Enter bx: ");
+// int y2 = ImputNum("Enter by: ");
+// int z2 = ImputNum("Enter bz: ");
+
+// System.Console.WriteLine(Math.Round(Math.Sqrt(Math.Pow((x2-x1), 2) + Math.Pow((y2-y1), 2) + Math.Pow((z2-z1), 2)), 2));
+
+
+// -----------Задача 23
+// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов 
+// чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+void PrintSquares(int x)
+{
+for (int i = 1; i <= x; i++)
+{
+System.Console.Write(Math.Pow(i, 3) + ", ");
+}
+}
+
+System.Console.WriteLine("Введите число N");
+int a = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine();
+PrintSquares(a);
