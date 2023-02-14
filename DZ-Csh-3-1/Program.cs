c6fdd635f0bc88a4cf23bcb0bfbc4cccc0f62103
Inna_IT <leto2077@gmail.com>
2023-02-14 23:58:28 +0300

    Решение задачи с 3го семинара

---
+++ DZ-Csh-3-1/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 19
+// Напишите программу, которая принимает на вход пятизначное число
+//  и проверяет, является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+// System.Console.WriteLine("Enter number : ");
+// string number = Console.ReadLine();
+// if (number is not null)
+// {
+//     if (number.Length == 5)
+//     {
+//         if ((number[0] == number[4]) && (number[1] == number[3]))
+//         {
+//             Console.WriteLine("Число является палиндромом");
+//         }
+//         else
+//         {
+//             Console.WriteLine("Число не является палиндром");
+//         }
+//     }
+//     else
+//     {
+//         Console.WriteLine("Введено не 5-значное число");
+//     }
+// }
